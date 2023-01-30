9d14fb85b6febc974e9b2a40beba76d24824b189
allisonalmeida <allisondealmeida@gmail.com>
2023-01-30 16:38:53 -0300

    update2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
-import './global.css';
+import { ThemeProvider } from 'styled-components';
+
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 export function App() {
-  return <h1>timer</h1>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>Timer</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
+  );
 }
