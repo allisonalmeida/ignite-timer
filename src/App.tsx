e33b6c70e77158f27e71db117f5b6ae3edf4d6e2
allisonalmeida <allisondealmeida@gmail.com>
2023-02-02 18:34:13 -0300

    update3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from 'styled-components';
+import { Button } from './components/Button';
 
 import { GlobalStyle } from './styles/global';
 import { defaultTheme } from './styles/themes/default';
@@ -6,7 +7,11 @@ import { defaultTheme } from './styles/themes/default';
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Timer</h1>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="success" />
+      <Button variant="danger" />
+      <Button />
 
       <GlobalStyle />
     </ThemeProvider>
